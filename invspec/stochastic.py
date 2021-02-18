b9a409d0be7e666815a768bd60ecaf9dbdfe4e55
rob arbon <robert.arbon@bristol.ac.uk>
2021-02-18 20:28:43 +0000

    added split matrix and split element functions

--- invspec/stochastic.py
+++ invspec/stochastic.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 stochastic.py
 Generates stochastic matrices with given properties
@@ -5,30 +7,62 @@ Generates stochastic matrices with given properties
 Handles the primary functions
 """
 
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+def stochastic(eigenvalues):
+    eigenvalues = get_valid_spectrum(eigenvalues)
 
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+
+
+def get_valid_spectrum(eigenvalues):
+    eigenvalues = np.sort(eigenvalues)[::-1]
+    if np.max(eigenvalues) >= 1:
+        raise ValueError('Eigenvalues must be strictly less that 1. Eigenvalue of 1 is included by default.')
+    if np.any(eigenvalues < 0):
+        raise NotImplemented('Negative eigenvalues not implemented yet.')
+    if np.any(np.iscomplex(eigenvalues)):
+        raise ValueError('Eigenvalues must be real.')
+    return eigenvalues
+
+
+def r_min(eigenvalue, matrix_element):
+    return np.max(0, eigenvalue/(eigenvalue+matrix_element))
+
+
+def r_max(eigenvalue, matrix_element):
+    return np.min(1, matrix_element/(eigenvalue + matrix_element))
+
+
+def split_element(eigenvalue, matrix_element):
+    if matrix_element > np.abs(eigenvalue):
+        raise ValueError('Matrix element to be split must be larger than proposed eigenvalue.')
+
+    min_val = r_min(eigenvalue, matrix_element)
+    max_val = r_max(eigenvalue, matrix_element)
+    r = np.random.uniform(min_val, max_val, size=1)[0]
+
+    split = (matrix_element/max_val)*np.array([[r, max_val-r],
+                                           [r-min_val, 1-r]])
+    return split, r
+
+
+def split_matrix(eigenvalue, matrix, matrix_element_ix):
+
+    A11 = matrix[:matrix_element_ix, :matrix_element_ix]
+    c1k = matrix[:matrix_element_ix, [matrix_element_ix]]
+    A12 = matrix[:matrix_element_ix, matrix_element_ix + 1:]
+
+    r1k = matrix[[matrix_element_ix], :matrix_element_ix]
+    r2k = matrix[[matrix_element_ix], matrix_element_ix + 1:]
+
+    A21 = matrix[matrix_element_ix + 1:, :matrix_element_ix]
+    c2k = matrix[matrix_element_ix + 1:, [matrix_element_ix]]
+    A22 = matrix[matrix_element_ix + 1:, matrix_element_ix + 1:]
+
+    matrix_element = matrix[matrix_element_ix, matrix_element_ix]
+    S, r = split_element(matrix_element, eigenvalue)
+
+    new_r1 = np.concatenate([A11, r * c1k, (1 - r) * c1k, A12], axis=1)
+    new_rk = np.concatenate([np.concatenate([r1k, r1k], axis=0), S, np.concatenate([r2k, r2k], axis=0)], axis=1)
+    new_r2 = np.concatenate([A21, r * c2k, (1 - r) * c2k, A22], axis=1)
+    new_A = np.concatenate([new_r1, new_rk, new_r2], axis=0)
+    return new_A
