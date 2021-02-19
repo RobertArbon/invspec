c2f60e8012c7df0d6bd20f9e884ceee236175936
rob arbon <robert.arbon@bristol.ac.uk>
2021-02-19 16:04:34 +0000

    produces single stochastic matrix

--- invspec/stochastic.py
+++ invspec/stochastic.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+TOL = 1E-10
 """
 stochastic.py
 Generates stochastic matrices with given properties
@@ -7,10 +7,23 @@ Generates stochastic matrices with given properties
 Handles the primary functions
 """
 
-def stochastic(eigenvalues):
+
+def stochastic_matrix(eigenvalues):
+
     eigenvalues = get_valid_spectrum(eigenvalues)
+    matrix = build_matrix(eigenvalues)
+    rmsd = eigenvalues_rmsd(eigenvalues, matrix)
+    if rmsd > TOL:
+        raise RuntimeError(f'Something has gone wrong! RMSD between requested and actual eigenvalues is {rmsd:4.2f}.')
+    else:
+        return matrix
 
 
+def eigenvalues_rmsd(eigenvalues, matrix):
+    new_eigenvalues = np.sort(np.linalg.eig(matrix)[0])[::-1]
+    new_eigenvalues_excl_1 = new_eigenvalues[1:]
+    rmsd = np.sqrt(np.mean((new_eigenvalues_excl_1 - eigenvalues) ** 2))
+    return rmsd
 
 
 def get_valid_spectrum(eigenvalues):
@@ -25,44 +38,58 @@ def get_valid_spectrum(eigenvalues):
 
 
 def r_min(eigenvalue, matrix_element):
-    return np.max(0, eigenvalue/(eigenvalue+matrix_element))
+    return max(0, eigenvalue/(eigenvalue + matrix_element))
 
 
 def r_max(eigenvalue, matrix_element):
-    return np.min(1, matrix_element/(eigenvalue + matrix_element))
+    return min(1, matrix_element/(eigenvalue + matrix_element))
 
 
 def split_element(eigenvalue, matrix_element):
-    if matrix_element > np.abs(eigenvalue):
-        raise ValueError('Matrix element to be split must be larger than proposed eigenvalue.')
+    if matrix_element < np.abs(eigenvalue):
+        raise ValueError(f'Matrix element ({matrix_element}) to be split must be larger than proposed eigenvalue '
+                         f'({eigenvalue}).')
 
     min_val = r_min(eigenvalue, matrix_element)
     max_val = r_max(eigenvalue, matrix_element)
     r = np.random.uniform(min_val, max_val, size=1)[0]
 
-    split = (matrix_element/max_val)*np.array([[r, max_val-r],
-                                           [r-min_val, 1-r]])
+    split = (matrix_element/max_val)*np.array([[r,         max_val-r],
+                                               [r-min_val,       1-r]])
     return split, r
 
 
-def split_matrix(eigenvalue, matrix, matrix_element_ix):
+def embiggen_matrix(eigenvalue, matrix, element_index):
+    A11 = matrix[:element_index, :element_index]
+    c1k = matrix[:element_index, [element_index]]
+    A12 = matrix[:element_index, element_index + 1:]
+    r1k = matrix[[element_index], :element_index]
+    r2k = matrix[[element_index], element_index + 1:]
+    A21 = matrix[element_index + 1:, :element_index]
+    c2k = matrix[element_index + 1:, [element_index]]
+    A22 = matrix[element_index + 1:, element_index + 1:]
 
-    A11 = matrix[:matrix_element_ix, :matrix_element_ix]
-    c1k = matrix[:matrix_element_ix, [matrix_element_ix]]
-    A12 = matrix[:matrix_element_ix, matrix_element_ix + 1:]
-
-    r1k = matrix[[matrix_element_ix], :matrix_element_ix]
-    r2k = matrix[[matrix_element_ix], matrix_element_ix + 1:]
-
-    A21 = matrix[matrix_element_ix + 1:, :matrix_element_ix]
-    c2k = matrix[matrix_element_ix + 1:, [matrix_element_ix]]
-    A22 = matrix[matrix_element_ix + 1:, matrix_element_ix + 1:]
-
-    matrix_element = matrix[matrix_element_ix, matrix_element_ix]
-    S, r = split_element(matrix_element, eigenvalue)
+    matrix_element = matrix[element_index, element_index]
+    S, r = split_element(eigenvalue, matrix_element)
 
     new_r1 = np.concatenate([A11, r * c1k, (1 - r) * c1k, A12], axis=1)
     new_rk = np.concatenate([np.concatenate([r1k, r1k], axis=0), S, np.concatenate([r2k, r2k], axis=0)], axis=1)
     new_r2 = np.concatenate([A21, r * c2k, (1 - r) * c2k, A22], axis=1)
     new_A = np.concatenate([new_r1, new_rk, new_r2], axis=0)
     return new_A
+
+
+def choose_element_to_split(eigenvalue, matrix):
+    matrix_diag = np.diag(matrix)
+    element_indices = np.where(matrix_diag > eigenvalue)[0]
+    element_index = np.random.choice(element_indices)
+    return element_index
+
+
+def build_matrix(eigenvalues):
+    matrix, _ = split_element(eigenvalues[0], 1)
+    for i in range(1, eigenvalues.shape[0]):
+        eigenvalue = eigenvalues[i]
+        element_index = choose_element_to_split(eigenvalue, matrix)
+        matrix = embiggen_matrix(eigenvalue, matrix, element_index)
+    return matrix
--- invspec/tests/test_invspec.py
+++ invspec/tests/test_invspec.py
@@ -4,9 +4,57 @@ Unit and regression test for the invspec package.
 
 # Import package, test suite, and other packages as needed
 import invspec
+import numpy as np
 import pytest
 import sys
+
 
 def test_invspec_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "invspec" in sys.modules
+
+
+def test_eigenvalues():
+    eigenvalues = np.array([0.9, 0.8])
+    mat = invspec.stochastic_matrix(eigenvalues)
+    eigenvalues = np.concatenate([[1], eigenvalues])
+
+    evs, _ = np.linalg.eig(mat)
+    evs = np.sort(evs)[::-1]
+    assert np.allclose(eigenvalues, evs)
+
+
+def is_stochastic(matrix):
+    row_sums_are_1 = np.allclose(np.sum(matrix, axis=1), np.ones(matrix.shape))
+    is_non_negative = np.all(matrix >= 0)
+    return row_sums_are_1 and is_non_negative
+
+
+def test_is_stochastic():
+    eigenvalues = np.array([0.9, 0.8])
+    mat = invspec.stochastic_matrix(eigenvalues)
+    assert is_stochastic(mat)
+
+
+def test_embiggen_eigenvalues():
+    matrix = np.array([[0.9, 0.1],
+                       [0.1, 0.9]])
+    evs, _ = np.linalg.eig(matrix)
+    new_ev = 0.7
+    new_matrix = invspec.embiggen_matrix(new_ev, matrix, element_index=0)
+    new_evs, _ = np.linalg.eig(new_matrix)
+    new_evs_correct = np.allclose(np.sort(np.concatenate([evs, [new_ev]])),
+                                  np.sort(new_evs))
+    assert new_evs_correct
+
+
+def test_embiggen_is_stochastic():
+    matrix = np.array([[0.9, 0.1],
+                       [0.1, 0.9]])
+    new_ev = 0.7
+    new_matrix = invspec.embiggen_matrix(new_ev, matrix, element_index=0)
+
+    assert is_stochastic(new_matrix)
+
+
+
